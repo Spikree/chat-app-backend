bf178a93b038399dde90b523a068011aed022fa6
spikree <avimahalingpure10@gmail.com>
2025-02-19 18:48:51 +0530

    created send message route and controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   const userId = req.user._id;
@@ -49,3 +50,40 @@ export const getMessages = async (req, res) => {
     })
   }
 };
+
+export const sendMessage = async (req,res) => {
+
+    const {text,image} = req.body;
+    const {id:reciverId} = req.params;
+    const senderId = req.user._id;
+
+    try {
+        let imageUrl;
+
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            reciverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        // yet to add real time functionality => socket.io
+
+        return res.status(201).json({
+            message: "message sent successfully",
+            newMessage
+        })
+    } catch (error) {
+        console.log("Error in send message controller");
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { ProtectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar,getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar,getMessages, sendMessage} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", ProtectRoute, getUsersForSidebar);
 router.get("/:id",ProtectRoute,getMessages)
+
+router.post("/send/:id", ProtectRoute,sendMessage)
 
 export default router;
